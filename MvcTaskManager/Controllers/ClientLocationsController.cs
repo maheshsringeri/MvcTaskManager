76404000f7aaf5c6305d8eca59082f6ed3fa4775
Mahesh <maheshsringeri@gmail.com>
2022-06-17 18:19:58 +0530

    After complete the client master changes

--- MvcTaskManager/Controllers/ClientLocationsController.cs
+++ MvcTaskManager/Controllers/ClientLocationsController.cs
@@ -25,5 +25,76 @@ namespace MvcTaskManager.Controllers
             List<ClientLocation> clientLocation = _db.ClientLocations.ToList();
             return Ok(clientLocation);
         }
+
+        [HttpGet]
+        [Route("api/clientlocations/searchbyclientlocationid/{ClientLocationID}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult GetByClientLocationID(int ClientLocationID)
+        {
+            ClientLocation clientLocation = _db.ClientLocations.FirstOrDefault(q => q.ClientLocationID == ClientLocationID);
+
+            if (clientLocation != null)
+            {
+                return Ok(clientLocation);
+            }
+            else
+            {
+                return NoContent();
+            }
+
+        }
+
+        [HttpPost]
+        [Route("api/clientlocations")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ClientLocation Post([FromBody] ClientLocation clientLocation)
+        {
+            _db.ClientLocations.Add(clientLocation);
+            _db.SaveChanges();
+
+            ClientLocation existingClientLocation1 = _db.ClientLocations.FirstOrDefault(q => q.ClientLocationID == clientLocation.ClientLocationID);
+            return existingClientLocation1;
+        }
+
+        [HttpPut]
+        [Route("api/clientlocations")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ClientLocation Put([FromBody] ClientLocation clientLocation)
+        {
+            ClientLocation existingClientLocation = _db.ClientLocations.FirstOrDefault(q => q.ClientLocationID == clientLocation.ClientLocationID);
+
+            if (existingClientLocation != null)
+            {
+                existingClientLocation.ClientLocationName = clientLocation.ClientLocationName;
+                _db.SaveChanges();
+
+                return existingClientLocation;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/clientlocations")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public int Delete(int ClientLoactionID)
+        {
+            ClientLocation existingClientLocation = _db.ClientLocations.FirstOrDefault(q => q.ClientLocationID == ClientLoactionID);
+
+            if (existingClientLocation != null)
+            {
+                _db.ClientLocations.Remove(existingClientLocation);
+                _db.SaveChanges();
+
+                return ClientLoactionID;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
     }
 }
